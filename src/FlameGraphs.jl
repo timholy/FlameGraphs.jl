37f2e30641337118ce1a464ec18650b0ce8e8b5b
Tim Holy <tim.holy@gmail.com>
2020-01-09 12:13:02 -0600

    Initial working commit

--- src/FlameGraphs.jl
+++ src/FlameGraphs.jl
@@ -1,5 +1,134 @@
 module FlameGraphs
 
-greet() = print("Hello World!")
+using Profile, LeftChildRightSiblingTrees
+using Base.StackTraces: StackFrame
+using Profile: StackFrameTree
+
+# AbstractTree interface for StackFrameTree:
+using AbstractTrees
+AbstractTrees.children(node::StackFrameTree) = node.down
+AbstractTrees.printnode(io::IO, node::StackFrameTree) = print(io, node.frame)
+
+export flamegraph
+
+struct NodeData
+    sf::StackFrame
+    status::UInt8             # a bitfield, see below
+    hspan::UnitRange{Int}
+end
+
+# status bitfield values
+const runtime_dispatch = UInt8(1)
+const gc_event         = UInt8(2)
+
+# This allows Revise to correct the location information in profiles
+if VERSION >= v"1.5.0-DEV.9"
+    # ref https://github.com/JuliaLang/julia/pull/34235
+    lineinfodict(data::Vector{UInt64}) = Profile.getdict(data)
+else
+    # Use the definition of Profile.getdict from Julia 1.5.0-DEV.9+
+    function lineinfodict(data::Vector{UInt64})
+        # Lookup is expensive, so do it only once per ip.
+        udata = unique(data)
+        dict = Profile.LineInfoDict()
+        for ip in udata
+            st = Profile.lookup(convert(Ptr{Cvoid}, ip))
+            # To correct line numbers for moving code, put it in the form expected by
+            # Base.update_stackframes_callback[]
+            stn = map(x->(x, 1), st)
+            try Base.invokelatest(Base.update_stackframes_callback[], stn) catch end
+            dict[UInt64(ip)] = map(first, stn)
+        end
+        return dict
+    end
+end
+
+"""
+    lidict = lineinfodict(uips)
+
+Look up location information for each instruction pointer in `uips`.
+This is a `Dict(UInt64=>Vector{StackFrame})`, where the `UInt64` is the instruction pointer.
+The reason a single instruction pointer gives a `Vector{StackFrame}` is because
+of inlining; the first entry corresponds to the instruction that actually ran,
+and the later entries correspond to the call chain.
+
+See also [`unique_ips`](@ref).
+"""
+lineinfodict(s::Set) = lineinfodict(collect(s))
+
+function flamegraph(data = Profile.fetch(); lidict=nothing, C=false, combine=true, recur=:off)
+    if lidict === nothing
+        lidict = lineinfodict(unique(data))
+    end
+    # # Clear out the unknown stackframes
+    # unknowns = Set{eltype(data)}()
+    # for (k, v) in lidict
+    #     iszero(k) && continue
+    #     if length(v) == 1
+    #         sf = v[1]
+    #         if sf == StackTraces.UNKNOWN || sf.line == sf.pointer
+    #             push!(unknowns, k)
+    #         end
+    #     end
+    # end
+    # @show unknowns
+    # keep = [ip ∉ unknowns for ip in data]
+    # data = data[keep]
+    root = combine ? StackFrameTree{StackFrame}() : StackFrameTree{UInt64}()
+    # Build the tree with C=true, regardless of user setting. This is because
+    # we need the C frames to set the status flag. They will be omitted by `flamegraph!`
+    # as needed.
+    if VERSION >= v"1.4.0-DEV.128"
+        root = Profile.tree!(root, data, lidict, #= C =# true, recur)
+    else
+        root = Profile.tree!(root, data, lidict, #= C =# true)
+    end
+    if isempty(root.down)
+        Profile.warning_empty()
+        return nothing
+    end
+    root.count = sum(pr->pr.second.count, root.down)  # root count seems borked
+    return flamegraph!(Node(NodeData(root.frame, status(root, C), 1:root.count)), root; C=C)
+end
+
+function status(node, C::Bool)
+    st = status(node.frame)
+    C && return st
+    # If we're suppressing C frames, check all C-frame children
+    for child in values(node.down)
+        child.frame.from_c || continue
+        st |= status(child, C)
+    end
+    return st
+end
+
+function status(sf::StackFrame)
+    st = UInt8(0)
+    if sf.from_c && (sf.func === :jl_invoke || sf.func === :jl_apply_generic)
+        st |= runtime_dispatch
+    end
+    if sf.from_c && startswith(String(sf.func), "jl_gc_")
+        st |= gc_event
+    end
+    return st
+end
+
+function flamegraph!(graph, ptree; C=false, hstart=first(graph.data.hspan))
+    nexts = collect(values(ptree.down))
+    lilist = collect(frame.frame for frame in nexts)
+    p = Profile.liperm(lilist)
+    for i in p
+        down = nexts[i]
+        frame, count = down.frame, down.count
+        if !C && frame.from_c
+            flamegraph!(graph, down; C=C, hstart=hstart)
+        else
+            child = addchild(graph, NodeData(frame, status(down, C), hstart:hstart+count-1))
+            flamegraph!(child, down; C=C)
+        end
+        hstart += count
+    end
+    return graph
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,115 @@
-using FlameGraphs
+using FlameGraphs, AbstractTrees
+using Base.StackTraces: StackFrame
 using Test
 
+# useful for testing
+stackframe(func, file, line; C=false) = StackFrame(Symbol(func), Symbol(file), line, nothing, C, false, 0)
+
 @testset "FlameGraphs.jl" begin
-    # Write your own tests here.
+    backtraces = UInt64[0, 4, 3, 2, 1,   # order: calles then caller
+                        0, 6, 5, 1,
+                        0, 8, 7,
+                        0, 4, 3, 2, 1,
+                        0]
+    lidict = Dict{UInt64,StackFrame}(1=>stackframe(:f1, :file1, 1),
+                                     2=>stackframe(:f2, :file1, 5),
+                                     3=>stackframe(:f3, :file2, 1),
+                                     4=>stackframe(:f2, :file1, 15),
+                                     5=>stackframe(:f4, :file1, 20),
+                                     6=>stackframe(:f5, :file3, 1),
+                                     7=>stackframe(:f1, :file1, 2),
+                                     8=>stackframe(:f6, :file3, 10))
+    g = flamegraph(backtraces; lidict=lidict)
+    @test all(node->node.data.status == 0, PostOrderDFS(g))
+    level1 = collect(g)
+    @test length(level1) == 2
+    n1, n2 = level1
+    @test n1.data.sf.func === :f1
+    @test n1.data.sf.line == 1
+    @test n1.data.hspan == 1:3
+    @test n2.data.sf.func === :f1
+    @test n2.data.sf.line == 2
+    @test n2.data.hspan == 4:4
+    level2a = collect(n1)
+    @test length(level2a) == 2
+    n3, n4 = level2a
+    @test n3.data.sf.func === :f2
+    @test n3.data.sf.line == 5
+    @test n3.data.hspan == 1:2
+    @test n4.data.sf.func === :f4
+    @test n4.data.sf.line == 20
+    @test n4.data.hspan == 3:3
+    level2b = collect(n2)
+    @test length(level2b) == 1
+    n5 = level2b[1]
+    @test n5.data.sf.func === :f6
+    @test n5.data.sf.line == 10
+    @test n5.data.hspan == 4:4
+    level3a = collect(n3)
+    @test length(level3a) == 1
+    n6 = level3a[1]
+    @test n6.data.sf.func === :f3
+    @test n6.data.sf.line == 1
+    @test n6.data.hspan == 1:2
+    level3b = collect(n4)
+    @test length(level3b) == 1
+    n7 = level3b[1]
+    @test n7.data.sf.func === :f5
+    @test n7.data.sf.line == 1
+    @test n7.data.hspan == 3:3
+    @test isempty(n5)
+    level4a = collect(n6)
+    @test length(level4a) == 1
+    n8 = level4a[1]
+    @test n8.data.sf.func === :f2
+    @test n8.data.sf.line == 15
+    @test n8.data.hspan == 1:2
+    @test isempty(n7)
+    @test isempty(n8)
+
+    # Now make some of them C calls
+    lidict = Dict{UInt64,StackFrame}(1=>stackframe(:f1, :file1, 1),
+                                     2=>stackframe(:jl_f, :filec, 55; C=true),
+                                     3=>stackframe(:jl_invoke, :file2, 1; C=true),
+                                     4=>stackframe(:f2, :file1, 15),
+                                     5=>stackframe(:f4, :file1, 20),
+                                     6=>stackframe(:f5, :file3, 1),
+                                     7=>stackframe(:f1, :file1, 2),
+                                     8=>stackframe(:f6, :file3, 10))
+    g = flamegraph(backtraces; lidict=lidict)
+    level1 = collect(g)
+    @test length(level1) == 2
+    n1, n2 = level1
+    @test n1.data.sf.func === :f1
+    @test n1.data.sf.line == 1
+    @test n1.data.hspan == 1:3
+    @test n1.data.status == FlameGraphs.runtime_dispatch
+    @test n2.data.sf.func === :f1
+    @test n2.data.sf.line == 2
+    @test n2.data.hspan == 4:4
+    @test n2.data.status == 0
+    level2a = collect(n1)
+    @test length(level2a) == 2
+    n3, n4 = level2a
+    @test n3.data.sf.func === :f4
+    @test n3.data.sf.line == 20
+    @test n3.data.hspan == 1:1
+    @test n4.data.sf.func === :f2
+    @test n4.data.sf.line == 15
+    @test n4.data.hspan == 2:3
+    level2b = collect(n2)
+    @test length(level2b) == 1
+    n5 = level2b[1]
+    @test n5.data.sf.func === :f6
+    @test n5.data.sf.line == 10
+    @test n5.data.hspan == 4:4
+    level3a = collect(n3)
+    @test length(level3a) == 1
+    n6 = level3a[1]
+    @test n6.data.sf.func === :f5
+    @test n6.data.sf.line == 1
+    @test n6.data.hspan == 1:1
+    @test isempty(n4)
+    @test isempty(n5)
+    @test isempty(n6)
 end
