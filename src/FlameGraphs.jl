a4898b81a156399c7d36b4961a78489d72a08c3c
Tim Holy <tim.holy@gmail.com>
2020-08-27 12:11:18 -0500

    Add a few more precompiles
    These knock about 0.2s off the first call to ProfileView.view

--- src/FlameGraphs.jl
+++ src/FlameGraphs.jl
@@ -18,4 +18,19 @@ include("render.jl")
 include("sfcategory.jl")
 include("io.jl")
 
+function _precompile_()
+    ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+    @assert precompile(flametags, (Node{NodeData}, Matrix{RGB{N0f8}}))
+    @assert precompile(flamegraph, ())
+    if isdefined(Base, :bodyfunction)
+        m = which(flamegraph, (Vector{UInt64},))
+        f = Base.bodyfunction(m)
+        @assert precompile(f, (Dict{UInt64,Vector{Base.StackTraces.StackFrame}}, Bool, Bool, Symbol, Bool, Vector{Tuple{Symbol,Symbol}}, Nothing, typeof(FlameGraphs.flamegraph), Vector{UInt64}))
+        m = which(flamepixels, (FlameColors, Node))
+        f = Base.bodyfunction(m)
+        @assert precompile(f, (Nothing, typeof(flamepixels), FlameColors, Node{NodeData}))
+    end
+end
+VERSION >= v"1.4.2" && _precompile_() # https://github.com/JuliaLang/julia/pull/35378
+
 end # module
