3577fe980293d09beac216b900852fdb389efc27
Ian Butterworth <i.r.butterworth@gmail.com>
2025-02-22 01:00:54 -0500

    add comment

--- src/render.jl
+++ src/render.jl
@@ -171,6 +171,8 @@ function flametags(g, img)
     tags = fill(1, axes(img))
     ndata = g.data
     sflist = StackFrame[StackTraces.UNKNOWN]
+    # use an IdDict so that equal stackframes from different places in the flamegraph can be
+    # distinguishable via egality
     sf2tag = IdDict{StackFrame,Int}(StackTraces.UNKNOWN=>1)
     tags[:,1] .= tagidx(ndata.sf, sf2tag, sflist)
     costscale = size(img, 1) / length(ndata.span)
